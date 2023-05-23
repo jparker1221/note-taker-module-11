ee6b27c648f0dfe374b8fa745fcaf65aaae2813b
jparker1221 <jonparker1221@gmail.com>
2023-05-23 16:52:44 -0400

    added get and post routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,30 @@
+const fs = require('fs');
+const uniqid = require('uniqid'); 
 const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        res.status(200).json(notes);
+    })
+})
+
+router.post('/notes', (req, res) => {
+    console.log(req.body)
+    // read the file then update the array then update the file with a new array
+    req.body.id = uniqid();
+    console.log(req.body)
+    fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+        notes.push(req.body);
+        console.log(notes);
+        fs.writeFile('./db/db.json', JSON.stringify(notes), (err, newData) => {
+        if (err) throw err;
+        res.status(200).json(notes);
+        })
+    })
+})
 
 module.exports = router;
